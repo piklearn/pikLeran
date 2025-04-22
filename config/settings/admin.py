b915135948ec5d3efdffdac4a440d9463d779a66
p1root <pkyanpwr@gmail.com>
2025-04-22 13:41:15 +0330

    add NavbarObtion model

--- config/settings/admin.py
+++ config/settings/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import SiteSettings
+from .models import SiteSettings, NavbarObtion
 
 @admin.register(SiteSettings)
 class SiteSettingsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(NavbarObtion)
+class NavbarObtion(admin.ModelAdmin):
     pass
--- config/settings/models.py
+++ config/settings/models.py
@@ -3,6 +3,21 @@ from django.db import models
 class SiteSettings(models.Model):
     setting_name = models.CharField(max_length=100, unique=True)
     setting_value = models.TextField()
+    class Meta:
+        verbose_name = "تنظیمات سایت"
+        verbose_name_plural = "تنظیمات سایت"
 
     def __str__(self):
         return self.setting_name
+    
+
+class NavbarObtion(models.Model):
+    name = models.CharField(max_length=50, verbose_name='نام')
+    link = models.URLField('آدرس')
+
+    class Meta:
+        verbose_name = "گزینه نوبار"
+        verbose_name_plural = "گزینه‌های نوبار"
+
+    def __str__(self):
+        return self.name
