243e22306a62d63ae8fe5a9da6a48b42aff121e7
p1root <89502688+p1root@users.noreply.github.com>
2026-02-26 18:30:05 +0330

    add auto complate to search box and some css for navbar items

--- config/courses/urls.py
+++ config/courses/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
-
 urlpatterns = [
-    path('<int:pk>', views.CourseDetailView.as_view(), name='detail')
+    path('<int:pk>', views.CourseDetailView.as_view(), name='detail'),
+    re_path(
+        r'^course-autocomplete/$',
+        views.CourseAutocomplete.as_view(),
+        name='course-autocomplete',
+    ),
 ]
--- config/courses/views.py
+++ config/courses/views.py
@@ -1,6 +1,26 @@
 from courses.models import Course
 from django.views.generic import DetailView
+from dal import autocomplete
 
+
+class CourseAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        # بررسی اینکه آیا کاربر وارد شده است
+        if not self.request.user.is_authenticated:
+            return Course.objects.none()
+
+        qs = Course.objects.all()  # به دست آوردن همه دوره‌ها
+
+
+        query = self.request.GET.get('query', '')  # به دست آوردن query از URL
+        if query:  
+            qs = qs.filter(title__icontains=query)
+        # فیلتر براساس ورودی کاربر
+        # print("query:",self.q)
+        # if self.q:  # اگر ورودی وجود داشته باشد
+        #     qs = qs.filter(title__icontains=self.q)  # جستجو در عنوان
+
+        return qs
 
 class CourseDetailView(DetailView):
     model = Course
